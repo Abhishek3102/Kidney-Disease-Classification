67b8f9d51d198a281300a82afed95672f6e7c243
Abhishek3102 <ankushpan694@gmail.com>
2025-01-03 23:04:20 +0530

    artifacts added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,17 +1,17 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
+from src.cnnClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
-from box import Configbox
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
 
 @ensure_annotations
-def read_yaml(path_to_yaml : Path) -> Configbox:
+def read_yaml(path_to_yaml : Path) -> ConfigBox:
     """reads yaml file and returns
 
     Args:
@@ -22,27 +22,27 @@ def read_yaml(path_to_yaml : Path) -> Configbox:
         e: empty file
 
     Returns:
-        Configbox: Configbox type
+        ConfigBox: ConfigBox type
     """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file : {path_to_yaml} loaded successfully")
-            return Configbox(content)
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
     
 @ensure_annotations
-def create_dictionaries(path_to_dictionaries: list, verbose:True):
+def create_directories(path_to_directory: list, verbose: bool = True):
     """create list of dictionaries
 
     Args:
-        path_to_dictionaries (list): list of path of dictionaries
+        path_to_directory (list): list of path of dictionaries
         ignore_log (bool, optional): ignore if multiple dirs is to be created.
     """
-    for path in path_to_dictionaries:
+    for path in path_to_directory:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
@@ -61,19 +61,19 @@ def save_json(path:Path, data:dict):
     logger.info(f"file saved at: {path}")
 
 @ensure_annotations
-def load_json(path: Path) -> Configbox:
+def load_json(path: Path) -> ConfigBox:
     """load json files data
     Args: 
         path (Path): path to json file
 
     Returns:
-        Configbox: data as class attributes instead of dict
+        ConfigBox: data as class attributes instead of dict
     """
     with open(path) as f:
         content = json.load(f)
 
     logger.info(f"json file loaded successfully from: {path}")
-    return Configbox(content)
+    return ConfigBox(content)
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
